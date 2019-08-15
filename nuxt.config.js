0426b0657bfa5edcc92a341cd206e3a37cb64eb1
basked <basket.baza@gmail.com>
2019-08-15 10:18:09 +0300

    Login Auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,83 @@
-
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' }
-    ]
-  },
-    script:[
-        {type:"text/javascript",  src:"https://code.jquery.com/jquery-3.3.1.slim.min.js" },
-        {type:"text/javascript",  src:"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" },
-        {type:"text/javascript",  src:"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" },
+    mode: 'universal',
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            {rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'}
+        ]
+    },
+    script: [
+        {type: "text/javascript", src: "https://code.jquery.com/jquery-3.3.1.slim.min.js"},
+        {type: "text/javascript", src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"},
+        {type: "text/javascript", src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"},
 
     ],
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  devModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** You can extend webpack config here
+    ** Customize the progress-bar color
     */
-    extend (config, ctx) {
+    loading: {color: '#fff'},
+    /*
+    ** Global CSS
+    */
+    css: [],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    devModules: [],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/auth'
+    ],
+    axioe: {
+        baseURL: 'http://basnuxtapi:3000/api/nuxt'
+    },
+
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        url: 'login',
+                        method: 'post',
+                        propertyName: 'meta.token'
+                    },
+                    user: {
+                        url: 'user',
+                        method: 'get',
+                        propertyName: 'data'
+                    },
+                    logout: {
+                        url: 'logout',
+                        method: 'post'
+                    }
+                }
+            }
+        }
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
     }
-  }
 }
---
+++ pages/login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container col-mt-6 mt-5">
+        <h2>Login</h2>
+        <br>
+        <form @submit.prevent="submit">
+            <div class="form-group">
+                <label>Email address</label>
+                <input v-model.trim="form.email" type="email" class="form-control" placeholder="Enter email" autofocus>
+                <small class="form-text text-danger">Show errors here</small>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input v-model.trim="form.password" type="password" class="form-control" placeholder="Password">
+                <small class="form-text text-danger">Show errors here</small>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Login</button>
+            <hr>
+            <br/>
+            <p>Dont have an account?
+                <nuxt-link to="/register">Register</nuxt-link>
+            </p>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data() {
+            return {
+                form: {
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            async submit() {
+                await this.$auth.loginWith('local', {
+                    data: this.form
+                })
+                this.$router.push('/');
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
